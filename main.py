7b674a65af7341d0bd15c75762f8203d45a8f53a
xunlin2 <xunlin2@illinois.edu>
2020-04-24 22:41:07 -0700

    put warmup into args

--- main.py
+++ main.py
@@ -105,7 +105,8 @@ def main(args, ITE=0):
     optimizer = torch.optim.SGD([{'params': model.parameters(), 'initial_lr': 0.03}], lr=args.lr, momentum=0.9, weight_decay=1e-4)
     # warm up schedule; scheduler_warmup is chained with schduler_steplr
     scheduler_steplr = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 14], gamma=0.1, last_epoch=-1)
-    scheduler_warmup = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=56, after_scheduler=scheduler_steplr)  # 20K=(idx)56, 35K=70 
+    if args.warmup:
+        scheduler_warmup = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=56, after_scheduler=scheduler_steplr)  # 20K=(idx)56, 35K=70 
     criterion = nn.CrossEntropyLoss() # Default was F.nll_loss; why test, train different?
 
     # Layer Looper
@@ -153,9 +154,9 @@ def main(args, ITE=0):
                 step = 0
             else:
                 original_initialization(mask, initial_state_dict)
-            optimizer = torch.optim.SGD([{'params': model.parameters(), 'initial_lr': 0.03}], lr=args.lr, momentum=0.9, weight_decay=1e-4)
-            scheduler_steplr = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 14], gamma=0.1, last_epoch=-1)
-            scheduler_warmup = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=56, after_scheduler=scheduler_steplr)  # 20K=(idx)56, 35K=70 
+            # optimizer = torch.optim.SGD([{'params': model.parameters(), 'initial_lr': 0.03}], lr=args.lr, momentum=0.9, weight_decay=1e-4)
+            # scheduler_steplr = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 14], gamma=0.1, last_epoch=-1)
+            # scheduler_warmup = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=56, after_scheduler=scheduler_steplr)  # 20K=(idx)56, 35K=70 
         print(f"\n--- Pruning Level [{ITE}:{_ite}/{ITERATION}]: ---")
 
         # Print the table of Nonzeros in each layer
@@ -183,7 +184,8 @@ def main(args, ITE=0):
             all_accuracy[iter_] = accuracy
             
             # warm up
-            scheduler_warmup.step()
+            if args.warmup:
+                scheduler_warmup.step()
             _lr = optimizer.param_groups[0]['lr']
 
             # Save the model during training
@@ -432,6 +434,7 @@ if __name__=="__main__":
     parser.add_argument("--prune_percent", default=10, type=int, help="Pruning percent")
     parser.add_argument("--prune_iterations", default=35, type=int, help="Pruning iterations count")
     parser.add_argument("--save_freq", default=10, type=int, help="Save frequency during training")
+    parser.add_argument("--warmup", default=False, type=bool, help="lr warmup")
 
     
     args = parser.parse_args()
